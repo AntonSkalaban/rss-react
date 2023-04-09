1dad44ac64483a05640dd4344b2ab48a3cb3a4e8
AntonSkalaban <112729189+AntonSkalaban@users.noreply.github.com>
2023-04-10 00:35:36 +0300

    feat: add some mock test

--- react-components/src/tests/CardsContainer.test.tsx
+++ react-components/src/tests/CardsContainer.test.tsx
@@ -1,13 +1,18 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import { getData } from 'pages/Main/requests/requests';
-// import { CardsContainer } from 'pages/Main/components/CardsContainer/CardsContainer';
-// import { Main } from 'pages/Main/Main';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, expect, test } from 'vitest';
+import { getData } from 'pages/Main/requests/requests';
+import { CardsContainer } from 'pages/Main/components/CardsContainer/CardsContainer';
+import { Main } from '../pages/Main/Main';
 
-// describe('Cards', () => {
-//   test('message', async () => {
-//     render(<Main />);
-//     expect(await findByText('Antenna')).toBeInTheDocument();
-//   });
-// });
+describe('Cards', () => {
+  test('message', async () => {
+    render(<Main />);
+    // userEvent.type(screen.getByPlaceholderText('Search here'), 'Antenna');
+    // userEvent.keyboard('{Enter}');
+
+    // await userEvent.keyboard('{Enter}');
+    expect(await screen.findByText(/Antenna Morty/i)).toBeInTheDocument();
+  });
+});
